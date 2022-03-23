dffb039d7bff0e0f2edff6a3f9fb31ffbbf70d65
suhy <704722924@qq.com>
2022-03-23 21:03:17 +0800

    主页框架完成

---
+++ 221900122_221900105/frondend/winter-olympics-statistics/src/views/homepage/components/h-goldlist.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="h-goldlist-container">
+        <h1>test goldlist</h1>
+        <div class="h-goldlist-card">
+          <div class="h-goldlist-card-num">
+
+          </div>
+          <div class="h-goldlist-card-country">
+            
+          </div>
+          <div class="h-goldlist-card-medal">
+            
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    data () {
+        return {}
+    }
+  }
+</script>
+<style>
+.h-goldlist-card{
+
+}
+
+</style>
