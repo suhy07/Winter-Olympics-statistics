052ba1bc9493388fe4b6ad39d7d9824bf633ba42
suhy <704722924@qq.com>
2022-03-24 16:04:07 +0800

    细节更新

---
+++ 221900122_221900105/frondend/winter-olympics-statistics/src/views/total/index.vue
@@ -0,0 +1,40 @@
+<template> 
+    <div class="total-container">
+        <div class="total-card total-body">
+            <div class="total-card total-title">
+                <h1>标题1111</h1>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import BaseHead from '@/components/base-head'
+export default{
+    data() {
+        return {
+        }
+    },
+    components: {
+        BaseHead,
+    }
+}
+</script>
+<style scoped>
+.total-container{
+    position: absolute;
+    top: 5px;
+    width:80.3%;
+}
+.total-card{
+    width: 100%;
+    height: 41px;
+    background: inherit;
+    background-color: rgba(255, 255, 255, 1);
+    box-sizing: border-box;
+    border-width: 1px;
+    border-radius: 188px;
+    box-shadow: 0px 0px 10px rgb(170 170 170 / 35%);
+    border-style: dotted;
+    border-color: rgb(0, 0, 0);
+}
+</style>
